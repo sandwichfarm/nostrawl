b5aea09615eb271e2968a5879904ede38d3edd1e
dskvr <dskvr@users.noreply.github.com>
2023-11-27 04:58:18 +0100

    Allow passing an Queue instance

--- src/adapters/BullMqTrawler.js
+++ src/adapters/BullMqTrawler.js
@@ -6,30 +6,18 @@ class BullMqTrawler extends Trawler {
   constructor(relays, options) {
     super(relays, options)
     this.$q = {}
-
-    this.defaults = {
-      connection: {
-        host: '127.0.0.1',
-        port: 6379,
-        db: 0
-      },
-      removeOnComplete: true, 
-      removeOnFail: true
-    }
+    this.opts()
   }
 
-  async init(){
-    const queueOpts = { ...this.defaults, ...this.options.queueOptions, connection: this.options.adapterOptions.redis }
-    console.log(this.options.queueName, queueOpts)
-    this.$q.queue = new Queue(this.options.queueName, queueOpts)
-    this.$q.worker = new Worker(
-        this.options.queueName, 
-        async $job => { 
-          console.log(`trawl job: worker ${$job.id} start`)
-          await this.trawl($job.data.chunk, $job)
-        }, 
-        { ...this.options.workerOptions, connection: this.options.adapterOptions.redis, concurrency: 1 }
-      )  
+  queue_init(){
+    let queueOpts 
+    if(this.options.queueOptions.$instance) {
+      this.$q = this.options.queueOptions.$instance
+    }
+    else {
+      queueOpts = { ...this.options.queueOptions, connection: this.options.adapterOptions.redis }
+      this.$q.queue = new Queue(this.options.queueName, queueOpts)
+    }
     this.$q.queue.on('active',      (...args) => this._on('queue_active',     ...args).catch(console.error))
     this.$q.queue.on('completed',   (...args) => this._on('queue_completed',  ...args).catch(console.error))
     this.$q.queue.on('failed',      (...args) => this._on('queue_failed',     ...args).catch(console.error))
@@ -37,7 +25,11 @@ class BullMqTrawler extends Trawler {
     this.$q.queue.on('waiting',     (...args) => this._on('queue_waiting',    ...args).catch(console.error))
     this.$q.queue.on('drained',     (...args) => this._on('queue_drained',    ...args).catch(console.error))  
     this.$q.queue.on('cleaned',     (...args) => this._on('queue_cleaned',    ...args).catch(console.error))
+  }
 
+  worker_init(){
+    this.$q.worker = new Worker(this.$q.name, async $job =>  await this.trawl($job.data.chunk, $job), this.options.workerOptions)  
+    
     this.$q.worker.on('active',     (...args) => this._on('worker_active',    ...args).catch(console.error))
     this.$q.worker.on('completed',  (...args) => this._on('worker_completed', ...args).catch(console.error))
     this.$q.worker.on('failed',     (...args) => this._on('worker_failed',    ...args).catch(console.error))
@@ -45,7 +37,30 @@ class BullMqTrawler extends Trawler {
     this.$q.worker.on('waiting',    (...args) => this._on('worker_waiting',   ...args).catch(console.error))
     this.$q.worker.on('drained',    (...args) => this._on('worker_drained',   ...args).catch(console.error))
     this.$q.worker.on('cleaned',    (...args) => this._on('worker_cleaned',   ...args).catch(console.error))
-    
+  }
+
+  opts(){
+    this.adapterDefaults = {
+      connection: {
+        host: '127.0.0.1',
+        port: 6379,
+        db: 0
+      } 
+    }
+    this.adapterOptions = { ...this.adapterDefaults, ...this.options.adapterOptions }
+
+    this.queueDefaults = { $instance: null }
+    this.queueOptions = { ...this.queueDefaults, ...this.options.queueOptions, connection: this.options.adapterOptions.redis }
+
+    this.workerDefaults = {
+      removeOnComplete: true,
+      removeOnFail: true
+    }
+    this.workerOptions = { ...this.workerDefaults, ...this.options.workerOptions, connection: this.options.adapterOptions.redis, concurrency: 1 }
+  }
+
+  async init(){
+    this.queue_init()
   }
 
   async updateProgress(progress, $job){
